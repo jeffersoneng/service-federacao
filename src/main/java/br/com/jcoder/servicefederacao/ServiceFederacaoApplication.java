e7425e2602bee87f79eb12845d7a7d13a89a20b1
jeffersoneng <jeffersoneng2005@gmail.com>
2021-12-31 18:07:00 -0300

    init

---
+++ src/main/java/br/com/jcoder/servicefederacao/ServiceFederacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.jcoder.servicefederacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceFederacaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceFederacaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/jcoder/servicefederacao/ServiceFederacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jcoder.servicefederacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceFederacaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
